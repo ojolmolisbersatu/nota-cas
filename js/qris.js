5092fc10734a5d71f2ec2e373e796e43e1da7b79
ojolmolisbersatu <zulfikrian.halal@gmail.com>
2026-09-08 02:02:43 +0700

    Add files via upload

--- js/qris.js
+++ js/qris.js
@@ -1,7 +1,7 @@
 // QRIS dinamis: menyisipkan nominal transaksi ke dalam kode QRIS statis,
 // mengikuti format EMV QR Code for Payment Systems (TLV) yang dipakai QRIS.
 // Referensi tag: 01=Point of Initiation Method, 53=Currency, 54=Transaction Amount,
-// 63=CRC. Static QRIS di bawah ini hasil decode dari gambar QRIS yang diupload.
+// 63=CRC. Static QRIS di bawah ini hasil decode dari gambar Semilir Semarang (NMID: ID1026583774124).
 
 const QRIS_STATIC =
   "00020101021126570011ID.DANA.WWW011893600915303471271802090347127180303UMI" +
